4c3f9b2f6b9ad082b017529ada321fe0aa8f6b6a
MikeMarelo <stels1040533@gmail.com>
2020-02-05 00:07:56 +0300

    new

---
+++ Assets/OpenQuestion.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OpenQuestion : MonoBehaviour
+{
+
+    public Question oobject;
+
+    public Text TextQuestion, TextAnswerOne, TextAnswerTwo;
+    public GameObject ButtonOne,ButtonTwo;
+    public Image imagecontaner;
+    void Start()
+    {
+        UpdateUi();
+    }
+    public void UpdateUi()
+    {
+
+        TextQuestion.text = oobject.text;
+        imagecontaner.sprite = oobject.Bckground;
+        if (oobject.Answers.Length > 0)
+        {
+            ButtonOne.SetActive(true);
+            TextAnswerOne.text = oobject.Answers[0].TitelForAnswer;
+            if (oobject.Answers.Length == 2)
+            {
+                ButtonTwo.SetActive(true);
+                TextAnswerTwo.text = oobject.Answers[1].TitelForAnswer;
+            }
+            else ButtonTwo.SetActive(false);
+
+        }
+        else
+        {
+            ButtonOne.SetActive(false);
+            ButtonTwo.SetActive(false);
+        }
+    }
+
+
+    public void OnClick(int index)
+    {
+        oobject = oobject.Answers[index];
+        UpdateUi();
+    }
+}
---
+++ Assets/Question.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Question",menuName = "Craeate New Question")]
+public class Question : ScriptableObject
+{
+    public string TitelForAnswer;
+    public string text;
+    public Sprite Bckground;
+    public Question[] Answers;
+}
